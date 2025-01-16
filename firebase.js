b3b043cc1721dad0b126cae2fdbb8f0f8b4f43be
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2025-01-16 12:41:40 -0800

    ✍(◔◡◔)

--- firebase.js
+++ firebase.js
@@ -1,11 +1,12 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
-import {  getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    onAuthStateChanged,
-  } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDUGNB_d1tXz8dCqMy0ooJ4nNee2mAFTzU",
@@ -21,90 +22,94 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
-// const db = getFirestore(app);
 
+
+
+/////   Initialize variables...
 let email = document.getElementById("email")
 let password = document.getElementById("pass")
 let signUpBttn = document.getElementById("signUpBttn")
-
-
-
-//// Creating Users Accounts! ///////////
-
-signUpBttn.addEventListener("click", async () => {
-await createUserWithEmailAndPassword(auth, email.value, password.value)
-  .then((userCredential) => {
-    // Signed up 
-    // email.value;
-    // password.value;
-    const user = userCredential.user;
-    let span1 = document.getElementById("spanSU")
-    span1.innerHTML = "SUCESSED!"
-    console.log("Account Created Successfully!")
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode, errorMessage)
-    console.log("Account Creation Failed")
-    // ..
-  });
-});
-
 
 let signInEmail = document.getElementById("signIn-email")
 let signInPass = document.getElementById("SignIn-pass")
 let signInbttn = document.getElementById("signInBttn")
 
+const user = auth.currentUser;
 
-/////////Signing In User's Account ////
-signInbttn.addEventListener("click", async () => {
-await signInWithEmailAndPassword(auth, signInEmail.value,  signInPass.value)
-.then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-  console.log("User Sign-In Successfully")
-   let span2 = document.getElementById("spanSI")
-    span2.innerHTML = "SUCESSED!"
-    // ...
-})
-.catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  console.log("Sign-in Failed")
-  console.log(errorCode, errorMessage)
-  });
+let mainDiv = document.getElementById("main");
+
+let mainContainerDiv = document.getElementById("main-container");
+let h1 = document.getElementById("h1");
+
+
+
+//// Creating Users Accounts! ///////////
+signUpBttn.addEventListener("click", async () => {
+  await createUserWithEmailAndPassword(auth, email.value, password.value)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      let span1 = document.getElementById("spanSU")
+      span1.innerHTML = "SUCESSED!"
+      console.log("Account Created Successfully!")
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage)
+      console.log("Account Creation Failed")
+      // ..
+    });
 });
 
 
 
-  /////////Checking If User Already Exists////
 
 
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-        console.log("User Logged In")
-      const uid = user.uid;
-    //   console.log(uid)
+/////////Signing In User's Account ////
+signInbttn.addEventListener("click", async () => {
+  await signInWithEmailAndPassword(auth, signInEmail.value, signInPass.value)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log("User Sign-In Successfully")
+      let span2 = document.getElementById("spanSI")
+      span2.innerHTML = "SUCESSED!"
       // ...
-    } else {
-      // User is signed out
-    
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("Sign-in Failed")
+      console.log(errorCode, errorMessage)
+    });
+});
+
+
+
+/////////Checking If User Already Exists////
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log("User Logged In")
+    const uid = user.uid;
+    mainDiv.style.display = "none";
+    mainContainerDiv.style.display = "block";
+    h1.innerHTML = email.value;
+
+  } else {
+    mainDiv.style.display = "block";
+    mainContainerDiv.style.display = "none";
     console.log("User Didn't Exists!");
-      // ...
-    }
-  });
+    // ...
+  }
+});
 
 
 
-  const user = auth.currentUser;
 
-if (user) {
-    console.log(user.value)
+// if (user) {
+//   console.log(user.value)
 
-} else {
-  // No user is signed in.
-}
+// } else {
+// }
 
-  
